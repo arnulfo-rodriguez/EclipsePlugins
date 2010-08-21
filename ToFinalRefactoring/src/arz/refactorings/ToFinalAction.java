8f48e844f2eed598ec73dbfa118f7aa546315a58
arnulfo <arnulfo@jesusfreak>
2010-08-21 09:10:23 -0600

    format fixes

--- ToFinalRefactoring/src/arz/refactorings/ToFinalAction.java
+++ ToFinalRefactoring/src/arz/refactorings/ToFinalAction.java
@@ -24,83 +24,85 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 @SuppressWarnings("restriction")
 public class ToFinalAction implements IWorkbenchWindowActionDelegate {
 
-	SourceField fField;
-	ICompilationUnit compilationUnit;
-
-	
+	private SourceField fField;
+	private ICompilationUnit fCompilationUnit;
 	private IWorkbenchWindow fWindow;
+
 	public void dispose() {
 		// Do nothing
 	}
-	
+
 	public void init(IWorkbenchWindow window) {
-		fWindow= window;
+		fWindow = window;
 	}
 
 	public void run(IAction action) {
-		if (fField != null && compilationUnit != null) {
-			ToFinalRefactoring refactoring= new ToFinalRefactoring();
+		if (fField != null && fCompilationUnit != null) {
+			ToFinalRefactoring refactoring = new ToFinalRefactoring();
 			refactoring.setField((SourceField) fField);
-			refactoring.setCompilationUnit(compilationUnit);
-			run(new ToFinalWizard(refactoring), fWindow.getShell(), "String to final");
+			refactoring.setCompilationUnit(fCompilationUnit);
+			run(new ToFinalWizard(refactoring), fWindow.getShell(),
+					"String to final");
 		}
 	}
-	
+
 	public void run(RefactoringWizard wizard, Shell parent, String dialogTitle) {
 		try {
-			RefactoringWizardOpenOperation operation= new RefactoringWizardOpenOperation(wizard);
+			RefactoringWizardOpenOperation operation = new RefactoringWizardOpenOperation(
+					wizard);
 			operation.run(parent, dialogTitle);
 		} catch (InterruptedException exception) {
 			// Do nothing
 		}
 	}
 
-    //I don't like having to use this method, i'd  rather make this work
-	//like the built in refactorings.
-	private ICompilationUnit CompilationUnitForCurrentEditor(){
+	// I don't like having to use this method, i'd rather make this work
+	// like the built in refactorings.
+	private ICompilationUnit CompilationUnitForCurrentEditor() {
 		IEditorPart editorSite = fWindow.getActivePage().getActiveEditor();
 		IEditorInput editorInput = editorSite.getEditorInput();
-		IResource resource = (IResource)editorInput.getAdapter(IResource.class);
-		ICompilationUnit icu = JavaCore.createCompilationUnitFrom((IFile)resource);
+		IResource resource = (IResource) editorInput
+				.getAdapter(IResource.class);
+		ICompilationUnit icu = JavaCore
+				.createCompilationUnitFrom((IFile) resource);
 		return icu;
 	}
-	
+
 	public void selectionChanged(IAction action, ISelection selection) {
-		fField= null;
-		compilationUnit = CompilationUnitForCurrentEditor();
+		fField = null;
+		fCompilationUnit = CompilationUnitForCurrentEditor();
 		if (selection instanceof IStructuredSelection) {
-			IStructuredSelection extended= (IStructuredSelection) selection;
-			Object[] elements= extended.toArray();
-			fField = getSelectedField(elements,compilationUnit);
-		}else if (selection instanceof ITextSelection){
+			IStructuredSelection extended = (IStructuredSelection) selection;
+			Object[] elements = extended.toArray();
+			fField = getSelectedField(elements, fCompilationUnit);
+		} else if (selection instanceof ITextSelection) {
 			try {
-				IJavaElement[] elements = SelectionConverter.codeResolve((IJavaElement) compilationUnit,(ITextSelection)selection);
-				 fField = getSelectedField(elements,compilationUnit); 
+				IJavaElement[] elements = SelectionConverter.codeResolve(
+						(IJavaElement) fCompilationUnit,
+						(ITextSelection) selection);
+				fField = getSelectedField(elements, fCompilationUnit);
 			} catch (JavaModelException e) {
 			}
 		}
-		
+
 		try {
-			action.setEnabled(fField != null && 
-					fField.exists() && 
-					fField.isStructureKnown() && 
-					!fField.getDeclaringType().isAnnotation() &&
-					Flags.isPrivate(fField.getFlags()) &&
-					!Flags.isFinal(fField.getFlags())
-					);
+			action.setEnabled(fField != null && fField.exists()
+					&& fField.isStructureKnown()
+					&& !fField.getDeclaringType().isAnnotation()
+					&& Flags.isPrivate(fField.getFlags())
+					&& !Flags.isFinal(fField.getFlags()));
 		} catch (JavaModelException exception) {
 			action.setEnabled(false);
 		}
 	}
 
-	private SourceField getSelectedField(Object[] elements,ICompilationUnit compilationUnit) {
+	private SourceField getSelectedField(Object[] elements,
+			ICompilationUnit compilationUnit) {
 		SourceField vField = null;
 		if (elements.length == 1 && elements[0] instanceof SourceField) {
-			vField= (SourceField) elements[0];
+			vField = (SourceField) elements[0];
 		}
 		return vField;
 	}
-	
-	
 
 }
