c3120755725c2ec96713c69ba999789f16ee0c5e
arnulfo <arnulfo@costarricense.cr>
2011-07-23 20:32:59 -0600

    Added null array

--- ToFinalRefactoring/src/arz/assists/ToFinalQuickAssistProcessor.java
+++ ToFinalRefactoring/src/arz/assists/ToFinalQuickAssistProcessor.java
@@ -4,7 +4,9 @@
 package arz.assists;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.IVariableBinding;
@@ -27,9 +29,42 @@ import arz.jdt.MakeFieldFinalDetector;
  */
 public class ToFinalQuickAssistProcessor implements IQuickAssistProcessor {
 
+	private static final IJavaCompletionProposal[] NULL_COMPLETION_PROPOSAL = new IJavaCompletionProposal[]{};
+
+	boolean hasToFinalQuickAssist(IBinding binding,
+			CompilationUnit compilationUnit) throws JavaModelException {
+		return (binding instanceof IVariableBinding && ((IVariableBinding) binding)
+				.isField())
+				&& MakeFieldFinalDetector.detect((IVariableBinding) binding,
+						compilationUnit);
+	}
+	
+	private ToFinalQuickAssistCompletionProposal buildToFinalCompletionProposal(
+			final IInvocationContext context, SimpleName simpleName,
+			IBinding binding) throws CoreException {
+		//I dont  perform any test here I KNOW is a sourceField
+		SourceField sourceField = (SourceField) binding.getJavaElement();
+
+		VariableDeclarationFragment declarationFragment = AstTools
+				.getDeclarationFragmentByName((FieldDeclaration) sourceField
+						.findNode(context.getASTRoot()), simpleName
+						.getIdentifier());
+
+		return  new ToFinalQuickAssistCompletionProposal(
+				"Make field final", context.getCompilationUnit(),
+				new FinalModifierAdder(context.getASTRoot().getAST(), context
+						.getCompilationUnit(), declarationFragment).addFinal(),
+				1, (Image) null);
+	}
+
+
 	@Override
 	public boolean hasAssists(IInvocationContext context) throws CoreException {
-		return false;
+		ASTNode coveringNode = context.getCoveringNode();
+		return (coveringNode instanceof SimpleName)
+				&& hasToFinalQuickAssist(
+						((SimpleName) coveringNode).resolveBinding(),
+						context.getASTRoot());
 	}
 
 	@Override
@@ -37,33 +72,17 @@ public class ToFinalQuickAssistProcessor implements IQuickAssistProcessor {
 			final IInvocationContext context, IProblemLocation[] locations)
 			throws CoreException {
 		ASTNode coveringNode = context.getCoveringNode();
+		
 		if (coveringNode instanceof SimpleName) {
+
 			SimpleName simpleName = (SimpleName) coveringNode;
 			IBinding binding = simpleName.resolveBinding();
-			if (binding instanceof IVariableBinding
-					&& ((IVariableBinding) binding).isField()) {
 
-				if (MakeFieldFinalDetector
-						.detect((IVariableBinding) binding,
-								context.getASTRoot())) {
-					
-					SourceField sourceField = (SourceField) binding.getJavaElement();
-					
-					VariableDeclarationFragment declarationFragment = AstTools.getDeclarationFragmentByName(
-							(FieldDeclaration) sourceField
-									.findNode(context.getASTRoot()),
-							simpleName.getIdentifier());
-					
-					return new IJavaCompletionProposal[] { new ToFinalQuickAssistCompletionProposal(
-							"Make field final", context.getCompilationUnit(),
-							new FinalModifierAdder(context.getASTRoot()
-									.getAST(), context.getCompilationUnit(),
-									 declarationFragment)
-									.addFinal(), 1, (Image) null) };
-				}
+			if (hasToFinalQuickAssist(binding, context.getASTRoot())) {
+				return new IJavaCompletionProposal[] { buildToFinalCompletionProposal(context, simpleName, binding)};
 			}
 		}
-		return null;
+		return NULL_COMPLETION_PROPOSAL;
 	}
 
 }
