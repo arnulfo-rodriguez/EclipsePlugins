20e1765e9c23c5e1e6af5e392f707a101e4a3863
hudson <hudson@jesusfreak2>
2013-03-13 03:19:12 -0600

    updating for juno

--- ToFinalRefactoring/src/arz/assists/ToFinalQuickAssistCompletionProposal.java
+++ ToFinalRefactoring/src/arz/assists/ToFinalQuickAssistCompletionProposal.java
@@ -2,17 +2,14 @@ package arz.assists;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
-import org.eclipse.jdt.internal.ui.text.correction.proposals.CUCorrectionProposal;
 import org.eclipse.jdt.ui.text.java.IInvocationContext;
+import org.eclipse.jdt.ui.text.java.correction.CUCorrectionProposal;
 import org.eclipse.ltk.core.refactoring.TextChange;
 import org.eclipse.swt.graphics.Image;
 
-import arz.jdt.AstTools;
 import arz.jdt.FinalModifierAdder;
 
-@SuppressWarnings("restriction")
 public class ToFinalQuickAssistCompletionProposal extends CUCorrectionProposal {
 
 	private ToFinalQuickAssistCompletionProposal(String name,
